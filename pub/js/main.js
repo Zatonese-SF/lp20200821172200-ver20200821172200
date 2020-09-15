b1e6edb49573deb5d78a50155b1bc542e39e68de
Zatonese <zatonese@gmail.com>
2020-09-16 03:14:24 +0900

    appinfo

--- pub/js/main.js
+++ pub/js/main.js
@@ -2,6 +2,19 @@
 
 {
 
+    function shuffle(arr) {
+        
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[j], arr[i]] = [arr[i], arr[j]];
+        }
+        return arr;
+    }
+
+
+
+
+
     // ▼ swiper ▼
 
         let mySwiper = new Swiper ('.swiper-container', {
@@ -25,20 +38,7 @@
 
 
 
-
-    function shuffle(arr) {
-        
-        for (let i = arr.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [arr[j], arr[i]] = [arr[i], arr[j]];
-        }
-        return arr;
-    }
-
-
-
     
-
     const tagName = [
 
         'あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお',
